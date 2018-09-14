a6d469daab8b2595ae34fe2b3592245f3b146c54
UnfortunatelyEvil <UnfortunatelyEvil@hotmail.com>
2018-09-14 14:59:46 -0400

    Initial

---
+++ VTMCalc-desktop/src/org/zcgames/vtmcalc/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.zcgames.vtmcalc.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.zcgames.vtmcalc.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
